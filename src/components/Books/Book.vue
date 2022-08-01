a1215375dba6efee35cf2ae976c03039157d2841
jigneshbhimani <jigneshbhimani2411@gmail.com>
2022-08-01 17:52:04 +0530

    search field add

--- src/components/Books/Book.vue
+++ src/components/Books/Book.vue
@@ -3,6 +3,13 @@
     <Header />
     <div class="content">
       <div class="container mb-5">
+        <!-- <input
+          type="text"
+          placeholder="Search Here..."
+          class="mt-3 search"
+          v-model="search"
+        /> -->
+
         <div class="row">
           <div
             @click="bookDetails(items)"
@@ -29,10 +36,33 @@ export default {
   components: {
     Header,
   },
+  data() {
+    return {
+      // search: null,
+    };
+  },
   computed: {
     books() {
       return this.$store.state.books;
     },
+    // resultSearch() {
+    //   if (this.search) {
+    //     return this.$store.state.books.filter((item) => {
+    //       return this.search
+    //         .toLowerCase()
+    //         .split(" ")
+    //         .every(
+    //           (v) =>
+    //             item.id.toString().includes(v) ||
+    //             item.name.toLowerCase().includes(v) ||
+    //             item.author.toLowerCase().includes(v) ||
+    //             item.published.toLowerCase().includes(v)
+    //         );
+    //     });
+    //   } else {
+    //     return this.$store.state.books;
+    //   }
+    // },
   },
   methods: {
     bookDetails(items) {
@@ -48,6 +78,10 @@ export default {
 <style scoped>
 .container {
   padding: 20px;
+}
+.search {
+  padding: 5px;
+  margin: 15px;
 }
 .image {
   width: 300px;
